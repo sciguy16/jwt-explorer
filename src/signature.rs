0838491c75b2cceac21c7a6ad77ae0fbb7605817
David Young <david@thedavidyoung.co.uk>
2021-10-24 20:19:34 +0100

    Implement ES384

--- src/signature.rs
+++ src/signature.rs
@@ -33,11 +33,11 @@ pub enum SignatureTypes {
     /// RSASSA-PKCS1-v1_5 using SHA-512
     Rs512,*/
     /// ECDSA using P-256 and SHA-256
-    Es256, /*
-           /// ECDSA using P-384 and SHA-384
-           Es384,
-           /// ECDSA using P-521 and SHA-512
-           Es512,
+    Es256,
+    /// ECDSA using P-384 and SHA-384
+    Es384,
+    /// ECDSA using P-521 and SHA-512
+    Es512, /*
            /// RSASSA-PSS using SHA-256 and MGF1 with SHA-256
            Ps256,
            /// RSASSA-PSS using SHA-384 and MGF1 with SHA-384
@@ -94,7 +94,7 @@ impl SignatureTypes {
         match self {
             None => Other,
             Hs256 | Hs384 | Hs512 => Hmac,
-            Es256 => Pubkey,
+            Es256 | Es384 | Es512 => Pubkey,
             Auto | Retain => {
                 if jwt_header.contains("HS") || jwt_header.contains("hs") {
                     return Hmac;
@@ -109,6 +109,17 @@ impl SignatureTypes {
                 Other
             }
         }
+    }
+
+    pub fn ec_group(&self) -> Option<EcGroup> {
+        use SignatureTypes::*;
+        let curve = match self {
+            Es256 => Nid::SECP256K1,
+            Es384 => Nid::SECP384R1,
+            Es512 => Nid::SECP521R1,
+            _ => return Option::None,
+        };
+        Some(EcGroup::from_curve_name(curve).unwrap())
     }
 }
 
@@ -154,10 +165,26 @@ pub fn calc_signature(
         }
         Es256 => {
             // ECDSA-256
+            //TODO factor out repeated code between Es* signatures
             let mut hasher = Sha256::new();
             hasher.update(payload.as_bytes());
             let payload = hasher.finalize();
             debug!("SHA256: {:02x?}", payload);
+            let secret_key = EcKey::private_key_from_pem(secret.as_bytes())?;
+            let signature = EcdsaSig::sign(&payload, &secret_key)?;
+            let mut signature_bytes = signature.r().to_vec();
+            debug!("r len: {}", signature_bytes.len());
+            signature_bytes.extend_from_slice(&signature.s().to_vec());
+            debug!("total len: {}", signature_bytes.len());
+
+            Ok(base64::encode_config(signature_bytes, URL_SAFE_NO_PAD))
+        }
+        Es384 => {
+            // ECDSA-384
+            let mut hasher = Sha384::new();
+            hasher.update(payload.as_bytes());
+            let payload = hasher.finalize();
+            debug!("SHA384: {:02x?}", payload);
             let secret_key = EcKey::private_key_from_pem(secret.as_bytes())?;
             let signature = EcdsaSig::sign(&payload, &secret_key)?;
             let mut signature_bytes = signature.r().to_vec();
@@ -179,7 +206,6 @@ pub fn verify_signature(
     hash_type: SignatureTypes,
 ) -> Result<bool> {
     use SignatureTypes::*;
-    const BN_LEN: usize = 32;
     match hash_type {
         Hs256 | Hs384 | Hs512 => {
             let calculated = calc_signature(payload, key, "", hash_type)?;
@@ -187,6 +213,9 @@ pub fn verify_signature(
         }
         Es256 => {
             // ECDSA-256
+            //TODO factor out repeated code
+
+            const BN_LEN: usize = 32;
 
             // Load the pubkey
             let pubkey = EcKey::public_key_from_pem(key.as_bytes())?;
@@ -212,6 +241,35 @@ pub fn verify_signature(
 
             Ok(sig.verify(&payload, &pubkey)?)
         }
+        Es384 => {
+            // ECDSA-384
+
+            const BN_LEN: usize = 48;
+
+            // Load the pubkey
+            let pubkey = EcKey::public_key_from_pem(key.as_bytes())?;
+
+            // Load the r and s components from the JWT signature
+            let sig_bytes = base64::decode_config(signature, URL_SAFE_NO_PAD)?;
+            if sig_bytes.len() != 2 * BN_LEN {
+                bail!(
+                    "signature of length {} is not {}",
+                    sig_bytes.len(),
+                    2 * BN_LEN
+                );
+            }
+            let r = BigNum::from_slice(&sig_bytes[..BN_LEN])?;
+            let s = BigNum::from_slice(&sig_bytes[BN_LEN..])?;
+            let sig = EcdsaSig::from_private_components(r, s)?;
+
+            // Hash the payload
+            let mut hasher = Sha384::new();
+            hasher.update(payload.as_bytes());
+            let payload = hasher.finalize();
+            debug!("SHA384: {:02x?}", payload);
+
+            Ok(sig.verify(&payload, &pubkey)?)
+        }
         None => Ok(true),
         _ => bail!("Unrecognised signature type: {}", hash_type),
     }
@@ -227,8 +285,10 @@ pub fn generate_keypair(signature_type: SignatureTypes) -> Result<KeyPair> {
     use SignatureTypes::*;
 
     match signature_type {
-        Es256 => {
-            let group = EcGroup::from_curve_name(Nid::SECP256K1)?;
+        Es256 | Es384 | Es512 => {
+            let group = signature_type
+                .ec_group()
+                .expect("Groups are all defined for relevant signatures");
             let kp = EcKey::generate(&group)?;
             Ok(KeyPair {
                 public: String::from_utf8(kp.public_key_to_pem()?)?,
@@ -252,7 +312,7 @@ mod test {
         use SignatureTypes::*;
         init();
 
-        for sig_type in &[Es256] {
+        for sig_type in &[Es256, Es384, Es512] {
             debug!("Signature type: {}", sig_type);
             let kp = generate_keypair(*sig_type).unwrap();
             debug!("Generated keypair:\n{:?}", kp);
@@ -375,6 +435,42 @@ OF/2NxApJCzGCEDdfSp6VQO30hyhRANCAAQRWz+jn65BtOMvdyHKcvjBeBSDZH2r
             &signature,
             pubkey,
             SignatureTypes::Es256,
+        )
+        .unwrap();
+        assert!(valid);
+    }
+
+    #[test]
+    fn es384() {
+        init();
+
+        let pubkey = r#"-----BEGIN PUBLIC KEY-----
+MHYwEAYHKoZIzj0CAQYFK4EEACIDYgAEC1uWSXj2czCDwMTLWV5BFmwxdM6PX9p+
+Pk9Yf9rIf374m5XP1U8q79dBhLSIuaojsvOT39UUcPJROSD1FqYLued0rXiooIii
+1D3jaW6pmGVJFhodzC31cy5sfOYotrzF
+-----END PUBLIC KEY-----"#;
+        let privkey = r#"-----BEGIN PRIVATE KEY-----
+MIG2AgEAMBAGByqGSM49AgEGBSuBBAAiBIGeMIGbAgEBBDCAHpFQ62QnGCEvYh/p
+E9QmR1C9aLcDItRbslbmhen/h1tt8AyMhskeenT+rAyyPhGhZANiAAQLW5ZJePZz
+MIPAxMtZXkEWbDF0zo9f2n4+T1h/2sh/fviblc/VTyrv10GEtIi5qiOy85Pf1RRw
+8lE5IPUWpgu553SteKigiKLUPeNpbqmYZUkWGh3MLfVzLmx85ii2vMU=
+-----END PRIVATE KEY-----"#;
+
+        let payload = concat!(
+            "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.",
+            "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYW",
+            "RtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0"
+        );
+
+        let signature =
+            calc_signature(payload, privkey, "", SignatureTypes::Es384)
+                .unwrap();
+
+        let valid = verify_signature(
+            payload,
+            &signature,
+            pubkey,
+            SignatureTypes::Es384,
         )
         .unwrap();
         assert!(valid);
